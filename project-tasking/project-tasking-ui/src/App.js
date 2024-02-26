20cb52358382907104e3883be7f89e4d71be4422
NathanGilbert6917 <nag6917@rit.edu>
2024-02-26 16:09:40 -0500

    Initial commit for frontend

---
+++ project-tasking/project-tasking-ui/src/App.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import axios from "axios";
+
+function App() {
+  function call() {
+    console.log("Calling");
+    axios
+      .get("http://localhost:8080/")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div onClick={call}>Call</div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

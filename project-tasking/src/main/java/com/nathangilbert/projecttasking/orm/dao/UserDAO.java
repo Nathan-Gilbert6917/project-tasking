f992f0900039e6d33c99e25e44387c6e62395da5
NathanGilbert6917 <nag6917@rit.edu>
2024-02-27 15:18:51 -0500

    Removed unused import

--- project-tasking/src/main/java/com/nathangilbert/projecttasking/orm/dao/UserDAO.java
+++ project-tasking/src/main/java/com/nathangilbert/projecttasking/orm/dao/UserDAO.java
@@ -7,7 +7,6 @@ import com.nathangilbert.projecttasking.rest.exceptions.UserNotFoundException;
 import com.nathangilbert.projecttasking.orm.entity.User;
 
 import jakarta.persistence.EntityManager;
-import jakarta.transaction.Transactional;
 
 @Repository
 public class UserDAO implements IUserDAO {
